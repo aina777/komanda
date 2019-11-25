082b553b40e145bcfe84bb9fc149dffdf0322007
aina777 <58169047+aina777@users.noreply.github.com>
2019-11-25 19:44:59 +0200

    pradinė struktūra

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Komanda</title>
+	
+	<?php include "head.php"; ?>
+
+</head>
+<body>
+
+<?php include "header.php"; ?>
+
+<h1>Pagrindinis</h1>
+	 
+
+
+
+
+
+	
+</body>
+</html>
