11b5f2300903ff17dc4b888843e95007e7d4e1c4
aina777 <58169047+aina777@users.noreply.github.com>
2019-11-26 11:34:30 +0200

    titulinio redagavimas

--- footer_1.php
+++ footer_1.php
@@ -1,25 +1,28 @@
  <footer class="page-footer">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="footer-copyright">
-            <div class="container">
-            © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-            </div>
-          </div>
-        </footer>
+  <div class="container">
+    <div class="row">
+      <div class="col l6 s12">
+        <h5 class="white-text">Kas ieško tas atranda</h5>
+        <p class="grey-text text-lighten-4">Kodas 000000</p>
+        <br>
+        <a target="_blank" class="grey-text text-lighten-3" href="https://lt.wikipedia.org/wiki/Kosmosas">Komandos kontaktai</a>              </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text">Naudinga info</h5>
+          <ul>
+            <li><a target="_blank" class="grey-text text-lighten-3" href="https://lt.wikipedia.org/wiki/Planeta!">Planetos</a></li>
+            <li><a target="_blank" class="grey-text text-lighten-3" href="https://lt.wikipedia.org/wiki/Kosmosas">Kosmosas</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+        © 2019 Komandos projektas
+        <a target="_blank" class="grey-text text-lighten-4 right" href="https://www.google.com/">Visagalis GOOGL'as</a>
+      </div>
+    </div>
+  </footer>
+
+
+
+
--- index.php
+++ index.php
@@ -8,34 +8,83 @@
 </head>
 <body>
 
-<?php include "header.php"; ?>
+	<?php include "header.php"; ?>
 
-<h1>Saulės sistema</h1>
-
-<img src="images/foto1.jpg">
-	 
+	<h1>Saulės sistema</h1>
 
 
- 
+	<div class="col s12 m7">
+		<div class="card horizontal">
+			<div class="card-image">
+				<img src="images/SaulesSistema.png">
+			</div>
+			<div class="card-stacked">
+				<div class="card-content">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+				<div class="card-action">
+					<a class= "blue-text" target="_blank" href="https://google.com">VISATA</a>		
+				</div>
+			</div>
+		</div>
+	</div>
 
-<div class="row">
-    <div class="col s12 red"><p>s12</p></div>
-    <div class="col s12 m4 l2 blue"><p>s12 m4</p></div>
-    <div class="col s12 m4 l8 orange"><p>s12 m4</p></div>
-    <div class="col s12 m4 l2 blac"><p>s12 m4</p></div>
-  </div>
-  <div class="row">
-    <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
-    <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
-    <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
-    <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
-  </div>
-
-<?php include "footer_1.php"; ?>
-
-<?php include "footer.php"; ?>
 
 
-	
+	<div class="konteineris">
+		<div class="aina1">
+			<img src="https://picsum.photos/200">
+			<h5>Žvaigždės</h5>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+			<a class="waves-effect waves-light btn blue" target="_blank" href="https://google.com"><i class="material-icons left">flare</i>Žvaigždės</a>
+		</div>
+		
+		<div class="aina1">
+			<img src="https://picsum.photos/200">
+			<h5>Planetos</h5>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+			<a class="waves-effect waves-light btn blue" target="_blank" href="https://google.com"><i class="material-icons left">nature_people</i>Planetos</a>
+		</div>
+		
+		<div class="aina1">
+			<img src="https://picsum.photos/200">
+			<h5>Palydovai</h5>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+			<a class="waves-effect waves-light btn blue" target="_blank" href="https://google.com"><i class="material-icons left">leak_add</i>Palydovai</a>
+		</div>
+		
+	</div>
+
+
+
+
+
+	<br>
+	<?php include "footer_1.php"; ?>
+
+	<?php include "footer.php"; ?>
+
+
+
 </body>
 </html>
