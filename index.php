7065e15710aff3bffce31c8a71d415d1f1ec65f0
aina777 <58169047+aina777@users.noreply.github.com>
2019-11-26 09:04:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,20 @@
 
 
  
+
+<div class="row">
+    <div class="col s12 red"><p>s12</p></div>
+    <div class="col s12 m4 l2 blue"><p>s12 m4</p></div>
+    <div class="col s12 m4 l8 orange"><p>s12 m4</p></div>
+    <div class="col s12 m4 l2 blac"><p>s12 m4</p></div>
+  </div>
+  <div class="row">
+    <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
+    <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
+    <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
+    <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
+  </div>
+
 <?php include "footer_1.php"; ?>
 
 <?php include "footer.php"; ?>
