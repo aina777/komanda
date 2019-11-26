ed5f1cc7bf51c089874f0abcd83a5cbf675ecd05
Dorimkute <58135167+Dorimkute@users.noreply.github.com>
2019-11-26 18:27:50 +0200

    Contacts

---
+++ contacts.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Komanda</title>
+	
+	<?php include "head.php"; ?>
+
+</head>
+<body>
+
+<?php include "header.php"; ?>
+
+
+<div>
+	<h1>Struktūra ir kontaktai</h1>
+
+  <div class="row">
+    <div class="col s12 m6 l3"> 
+		<img src="https://picsum.photos/200" alt="" class="circle responsive-img">
+		<h4>Direktorius <br> Rokas Gervinskas</h4>
+		<p>Kontaktinis tel. +3706*******. <br> Elektronis paštas *****@gmail.com</p>
+	</div>
+
+    <div class="col s12 m6 l3">
+    	<img src="https://picsum.photos/200" alt="" class="circle responsive-img">
+		<h4>Komunikacijos vadovė <br> Aina Jonuskytė</h4>
+		<p>Kontaktinis tel. +3706*******. <br> Elektronis paštas *****@gmail.com</p>
+    </div>
+
+     <div class="col s12 m6 l3">
+    	<img src="https://picsum.photos/200" alt="" class="circle responsive-img">
+		<h4>Lazerių pardavimų vadovas <br> Nerijus Bauras</h4>
+		<p>Kontaktinis tel. +3706*******. <br> Elektronis paštas *****@gmail.com</p>
+    </div>
+
+     <div class="col s12 m6 l3">
+    	<img src="https://picsum.photos/200" alt="" class="circle responsive-img">
+		<h4>Kosmoso specialistė <br> Dovilė Rimkutė</h4>
+		<p>Kontaktinis tel. +3706*******. <br> Elektronis paštas *****@gmail.com</p>
+    </div>
+   
+</div>
+
+
+
+
+<?php include "footer.php"; ?>
+	
+</body>
+</html>
