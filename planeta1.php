98826c7a8a225468b1617ce7b114e2e3a95acf24
Deaps <rikiz17@gmail.com>
2019-11-26 21:16:39 +0200

    Planetaaaa

--- planeta1.php
+++ planeta1.php
@@ -5,15 +5,16 @@
 	
 	<?php include "head.php"; ?>
 
+
 </head>
-<body>
+<body class="marsiukas">
 
 <?php include "header.php"; ?>
 
 <h1 class="theone">The Red Planet - Mars</h1>
 	 
 
-<img class="mars" src="images/mars1.jpg">
+<img class="mars" src="images/mars2.jpg">
 
 <div class="thetwo">
 
@@ -23,39 +24,60 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<button type="button" class="collapsible">Open Collapsible</button>
+<div class="content">
+  <p><img src="https://i.kym-cdn.com/photos/images/original/000/296/751/f97.jpg"></p>
+</div>
  
     <div class="row">
 
       <div class="col s4">
+      	<a target="_blank" href="https://nineplanets.org/mars/">
+      	<img class="marsas" src="images/mars3.jpg">
+      	</a>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<a target="_blank" class="waves-effect waves-light btn" href="https://nineplanets.org/mars/"><i class="material-icons left">search</i>Do you wanna know more?</a>
+
       </div>
       <div class="col s4">
+      	<a href="https://www.nationalgeographic.com/science/space/space-exploration/mars-exploration-article/">
+      	<img class="marsas" src="images/mars4.jpg">
+      	</a>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+        <a target="_blank" class="waves-effect waves-light btn" href="https://www.nationalgeographic.com/science/space/space-exploration/mars-exploration-article/"><i class="material-icons left">search</i>Do you wanna know more?</a>
+
       </div>
       <div class="col s4">
+      	<a href="https://mars.nasa.gov/mars-exploration/missions/mars2020/">
+      	<img class="marsas" src="images/mars5.jpg">
+      	</a>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <a target="_blank" class="waves-effect waves-light btn" href="https://mars.nasa.gov/mars-exploration/missions/mars2020/"><i class="material-icons left">search</i>Do you wanna know more?</a>
       </div>
 
     </div>
 
 </div>
 
-<div class="row">
+<!-- <div class="row">
     <div class="col s12 red"><p>s12</p></div>
     <div class="col s12 m4 l2 yellow"><p>s12 m4</p></div>
     <div class="col s12 m4 l8 black"><p>s12 m4</p></div>
@@ -68,7 +90,7 @@ proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <div class="col s12 m6 l3 green"><p>s12 m6 l3</p></div>
   </div>
 
-
+ -->
 
 
 <?php include "footer_1.php"; ?>
