3f66e70b28c260dced3db766b9e33adf902c2760
aina777 <58169047+aina777@users.noreply.github.com>
2019-11-27 20:50:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,69 +10,71 @@
 
 	<?php include "header.php"; ?>
 
-<div class="container">
-	<h1>Saulės sistema</h1>
+	<div class="container">
+		<h1>Saulės sistema</h1>
 
 
-	<div class="col s12 m7">
-		<div class="card horizontal">
-			<div class="card-image">
-				<img src="images/SaulesSistema.png">
-			</div>
-			<div class="card-stacked">
-				<div class="card-content">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>					
+		<div class="col s12 m7">
+			<div class="card horizontal">
+				<div class="card-image">
+					<img src="images/SaulesSistema.png">
 				</div>
-				<div class="card-action">
-					<a class= "blue-text" target="_blank" href="https://google.com">VISATA</a>		
+				<div class="card-stacked">
+					<div class="card-content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>					
+					</div>
+					<div class="card-action">
+						<a class= "blue-text" target="_blank" href="https://google.com">VISATA</a>		
+					</div>
 				</div>
 			</div>
 		</div>
+
+
+
+		<div class="konteineris">
+			<div class="aina1">
+				<img src="https://picsum.photos/200">
+				<h5>Žvaigždės</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+				<a class="waves-effect waves-light btn blue" target="_blank" href="https://google.com"><i class="material-icons left">flare</i>Žvaigždės</a>
+			</div>
+
+			<div class="aina1">
+				<img src="https://picsum.photos/200">
+				<h5>Planetos</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+				<a class="waves-effect waves-light btn blue" target="_blank" href="https://google.com"><i class="material-icons left">nature_people</i>Planetos</a>
+			</div>
+
+			<div class="aina1">
+				<img src="https://picsum.photos/200">
+				<h5>Palydovai</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+				<a class="waves-effect waves-light btn blue" target="_blank" href="https://google.com"><i class="material-icons left">leak_add</i>Palydovai</a>
+			</div>
+
+		</div>
+
 	</div>
 
 
 
-	<div class="konteineris">
-		<div class="aina1">
-			<img src="https://picsum.photos/200">
-			<h5>Žvaigždės</h5>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-			<a class="waves-effect waves-light btn blue" target="_blank" href="https://google.com"><i class="material-icons left">flare</i>Žvaigždės</a>
-		</div>
-		
-		<div class="aina1">
-			<img src="https://picsum.photos/200">
-			<h5>Planetos</h5>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-			<a class="waves-effect waves-light btn blue" target="_blank" href="https://google.com"><i class="material-icons left">nature_people</i>Planetos</a>
-		</div>
-		
-		<div class="aina1">
-			<img src="https://picsum.photos/200">
-			<h5>Palydovai</h5>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-			<a class="waves-effect waves-light btn blue" target="_blank" href="https://google.com"><i class="material-icons left">leak_add</i>Palydovai</a>
-		</div>
-		
-	</div>
 
-
-
-	</div>
 
 	<?php include "footer_1.php"; ?>
 
@@ -85,7 +87,7 @@
 		<li class="waves-effect"><a href="http://localhost/komanda/contacts.php">3</a></li>
 		<li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
 	</ul>
- -->
+-->
 
 
 </body>
