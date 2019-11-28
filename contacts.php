c4f86386a19990b00b9fe87967cfbe633de4d7b6
Dorimkute <58135167+Dorimkute@users.noreply.github.com>
2019-11-28 21:19:45 +0200

    paskutinis

--- contacts.php
+++ contacts.php
@@ -8,74 +8,83 @@
 </head>
 <body class="belekas">
 
-<?php include "header.php"; ?>
+	<?php include "header.php"; ?>
 
+	<div class="container">
 
-<div>
-	<h1 class="dovi">Struktūra ir kontaktai</h1>
+		<!-- <div> -->
+			<h1 class="dovi">Struktūra ir kontaktai</h1>
 
-  <div class="row">
-  	<div class = "dovile">
-    <div class="col s12 m6 l3"> 
-		<img src="images/kosmonautas2.jpg" alt="" class="circle responsive-img">
-		<h5>Direktorius </h5> 
-		<h6>Rokas Gervinskas</h6>
-		<p>Kontaktinis tel. +3706*******. <br> Elektronis paštas *****@gmail.com</p>
 	</div>
+			<div class="zmones">
+			<!-- <div class="dovile"> -->
 
-    <div class="col s12 m6 l3">
-    	<img src="images/aina2.jpg" alt="" class="circle responsive-img">
-		<h5>Komunikacijos vadovė </h5>
-		<h6>Aina Jonuškytė</h6>
-		<p>Kontaktinis tel. +3706*******. <br> Elektronis paštas *****@gmail.com</p>
-    </div>
+			<div class="juosta"></div>
+			<div class="container relatyvus">
+			<div class="row dovile">
+					<div class="col s12 m6 l3"> 
+						<img src="images/kosmonautas2.jpg" alt="" class="circle responsive-img">
+						<h5>Direktorius </h5> 
+						<h6>Rokas Gervinskas</h6>
+						<p class="pastraipa">Kontaktinis tel. +3706*******. <br> El. paštas *****@gmail.com</p>
+					</div>
 
-     <div class="col s12 m6 l3">
-    	<img src="images/kosmonautas.jpg" alt="" class="circle responsive-img">
-		<h5>Kosmonautas </h5> 
-		<h6>Nerijus Bauras</h6>
-		<p>Kontaktinis tel. +3706*******. <br> Elektronis paštas *****@gmail.com</p>
-    </div>
+					<div class="col s12 m6 l3">
+						<img src="images/aina2.jpg" alt="" class="circle responsive-img">
+						<h5>Komunikacijos vadovė</h5>
+						<h6>Aina Jonuškytė</h6>
+						<p class="pastraipa">Kontaktinis tel. +3706*******. <br> El. paštas *****@gmail.com</p>
+					</div>
 
-     <div class="col s12 m6 l3">
-    	<img src="images/specialiste.jpg" alt="" class="circle responsive-img">
-		<h5>Kosmoso specialistė </h5> 
-		<h6>Dovilė Rimkutė </h6>
-		<p>Kontaktinis tel. +3706*******. <br> Elektronis paštas *****@gmail.com</p>
-    </div>
-   </div>
-</div>
+					<div class="col s12 m6 l3">
+						<img src="images/kosmonautas.jpg" alt="" class="circle responsive-img">
+						<h5>Kosmonautas </h5> 
+						<h6>Nerijus Bauras</h6>
+						<p class="pastraipa">Kontaktinis tel. +3706*******. <br> El. paštas *****@gmail.com</p>
+					</div>
 
-<a target="_blank" href="https://www.instagram.com/">
-<h5 class="instagram">FOLLOW US ON INSTAGRAM</h5>
-</a>
+					<div class="col s12 m6 l3">
+						<img src="images/specialiste.jpg" alt="" class="circle responsive-img">
+						<h5>Kosmoso specialistė </h5> 
+						<h6>Dovilė Rimkutė </h6>
+						<p class="pastraipa">Kontaktinis tel. +3706*******. <br> El. paštas *****@gmail.com</p>
+					</div>
+				</div>
+				</div>
+			</div>
 
-<div class="insta" >
- <img class="pictas" src="images/galaktika.jpg">
- <h5 class="#cfd8dc-text">@KOSMOSASVISATOJE</h5>
-</div>
+		
 
+			<a target="_blank" href="https://www.instagram.com/">
+				<h5 class="instagram">FOLLOW US ON INSTAGRAM</h5>
+			</a>
 
+			<div class="insta" >
+				<img class="pictas" src="images/galaktika.jpg">
+				<h5 class="black-text">@KOSMOSASVISATOJE</h5>
+			</div>
 
-<div class="row">
-	<div class="blokas">
-    		<div class="col s12 l4">
-    			<p>Mus rasite adresu: Vilnius, Ulonų g. 5, LT-07160 <br>Darbo laikas: I-V 10-19 
-    			<br>Telefonas +370 6555 5555 <br>El. paštas visiskasKosmosas@gmail.com</p> 
-    			 <div class="video-container">
-        				<iframe width="853" height="480" src="https://www.youtube.com/embed/y4nMYQhC6KA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-     			 </div>
-    		</div>
-		<div class="col s12 l8">
-    		<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.983423849861!2d25.29012821534312!3d54.70991477940253!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96ae1dedd637%3A0x73978617430c730c!2sUlon%C5%B3%20g.%205%2C%20Vilnius%2008240!5e0!3m2!1slt!2slt!4v1574788973275!5m2!1slt!2slt" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-    	</div>
-    </div>
-</div>
+			<div class="container">
 
+			<div class="row">
+				<div class="blokas">
+					<div class="col s12 l4">
+						<p>Mus rasite adresu: Vilnius, Ulonų g. 5, LT-07160 <br>Darbo laikas: I-V 10-19 
+							<br>Telefonas +370 6555 5555 <br>El. paštas visiskasKosmosas@gmail.com</p> 
+							<div class="video-container">
+								<iframe width="853" height="480" src="https://www.youtube.com/embed/y4nMYQhC6KA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							</div>
+						</div>
+						<div class="col s12 l8">
+							<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.983423849861!2d25.29012821534312!3d54.70991477940253!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96ae1dedd637%3A0x73978617430c730c!2sUlon%C5%B3%20g.%205%2C%20Vilnius%2008240!5e0!3m2!1slt!2slt!4v1574788973275!5m2!1slt!2slt" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+						</div>
+					</div>
+				</div>
 
+			</div>
 
-<?php include "footer_1.php"; ?>
-<?php include "footer.php"; ?>
-	
-</body>
-</html>
+			<?php include "footer_1.php"; ?>
+			<?php include "footer.php"; ?>
+
+		</body>
+		</html>
